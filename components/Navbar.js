1719d8b6c09c5877a5056cde79ac0d43d938bf71
Brayden Pettigrew <brayden@thepettigrews.org>
2024-09-15 18:55:49 -0400

    Changed <a> -> <Link>

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,116 +1,111 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import Link from "next/link";
 
-function Navbar(){
+function Navbar() {
+  return (
+    <NavbarSection>
+      <NavbarSlideSmart>
+        <Link href="/">SlideSmart</Link>
+      </NavbarSlideSmart>
 
-    return(
-        <NavbarSection>
+      <NavbarAboutLinks>
+        <Link href="/">Saved Projects</Link>
+        <Link href="/">How it Works</Link>
+        <Link href="/">Support</Link>
+        <Link href="/">Why SlideSmart</Link>
+      </NavbarAboutLinks>
 
-            <NavbarSlideSmart>
-                <a href="/">SlideSmart</a>
-            </NavbarSlideSmart>
-
-            <NavbarAboutLinks>
-                <a href="/" >Saved Projects</a>
-                <a href="/" >How it Works</a>
-                <a href="/" >Support</a>
-                <a href="/" >Why SlideSmart</a>
-            </NavbarAboutLinks>
-            
-
-            <NavbarLoginLinks>
-                    <NavbarLoginStyle><a href="/">Login</a></NavbarLoginStyle>
-                    <NavbarRegisterStyle><a href="/">Register</a></NavbarRegisterStyle>
-            </NavbarLoginLinks>
-
-        </NavbarSection>
-    )
+      <NavbarLoginLinks>
+        <NavbarLoginStyle>
+          <Link href="/">Login</Link>
+        </NavbarLoginStyle>
+        <NavbarRegisterStyle>
+          <Link href="/">Register</Link>
+        </NavbarRegisterStyle>
+      </NavbarLoginLinks>
+    </NavbarSection>
+  );
 }
 
 const NavbarAboutLinks = styled.div`
-    margin-left: 50px;
-    display: flex;
-    justify-content: space-between;
-    gap: 30px;
-    
+  margin-left: 50px;
+  display: flex;
+  justify-content: space-between;
+  gap: 30px;
 
-    a {
-        text-decoration: none;
-        color: inherit;
-        transition: color 0.3s;
-        font-size: 20px;
-        font-weight: bold;
-    }
+  a {
+    text-decoration: none;
+    color: inherit;
+    transition: color 0.3s;
+    font-size: 20px;
+    font-weight: bold;
+  }
 
-    a:hover {
-        color: #F03A47;
-    }
-`
+  a:hover {
+    color: #f03a47;
+  }
+`;
 
 const NavbarLoginStyle = styled.div`
-    margin-left: 16px;
-    padding: 6px;
+  margin-left: 16px;
+  padding: 6px;
 
-    a {
-        text-decoration: none;
-        color: inherit;
-        transition: color 0.3s;
-        font-size: 25px;
-        font-weight: bold;
-    }
+  a {
+    text-decoration: none;
+    color: inherit;
+    transition: color 0.3s;
+    font-size: 25px;
+    font-weight: bold;
+  }
 
-    a:hover {
-        color: #F03A47;
-    }
-
-`
+  a:hover {
+    color: #f03a47;
+  }
+`;
 
 const NavbarRegisterStyle = styled.div`
-    margin-left: 16px;
+  margin-left: 16px;
+  padding: 6px;
+
+  a {
+    text-decoration: none;
+    color: white;
+    transition: color 0.3s;
+    font-size: 25px;
+    font-weight: bold;
+    border-radius: 8px;
+    background-color: #f03a47;
     padding: 6px;
+    border: 2px solid #f03a47;
+  }
 
-    a {
-        text-decoration: none;
-        color: white;
-        transition: color 0.3s;
-        font-size: 25px;
-        font-weight: bold;
-        border-radius: 8px;
-        background-color: #F03A47;
-        padding: 6px;
-        border: 2px solid #F03A47;
-
-    }
-
-    a:hover {
-        color: black;
-    }
-
-`
+  a:hover {
+    color: black;
+  }
+`;
 
 const NavbarLoginLinks = styled.div`
-    float: right;
-    margin-left: auto;
-    padding: 6px;
-    display: flex;
-    font-size: 25px;
-    color: #000000;
-    font-weight: bold;
-    
-`
+  float: right;
+  margin-left: auto;
+  padding: 6px;
+  display: flex;
+  font-size: 25px;
+  color: #000000;
+  font-weight: bold;
+`;
 
 const NavbarSlideSmart = styled.div`
-    float: left;
-    padding: 12px;
+  float: left;
+  padding: 12px;
 
-    a {
-        text-decoration: none;
-        font-size: 40px;
-        color: #000000;
-        font-weight: bold;
-
-    }
-`
+  a {
+    text-decoration: none;
+    font-size: 40px;
+    color: #000000;
+    font-weight: bold;
+  }
+`;
 
 const NavbarSection = styled.div`
     display: flex;
@@ -120,6 +115,6 @@ const NavbarSection = styled.div`
     background-color: #f6f4f3;
     padding: 20px;
 
-`
+`;
 
-export default Navbar
+export default Navbar;
