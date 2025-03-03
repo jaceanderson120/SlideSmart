cb8cb5cfde811d42ab4be63859f2f004626081e0
Brayden Pettigrew <brayden@thepettigrews.org>
2025-03-03 10:31:18 -0500

    Added admin functionality for auto-Spark plan

--- utils/getSparkStatus.ts
+++ utils/getSparkStatus.ts
@@ -1,12 +1,29 @@
 import { FirebaseApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { collection, onSnapshot, query, where } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
 import { db } from "@/firebase/firebase";
 
 export const getSparkStatus = async (app: FirebaseApp) => {
   const auth = getAuth(app);
   const userId = auth.currentUser?.uid;
   if (!userId) throw new Error("User not logged in");
+
+  // Check the usersRef to see if the user has a free subscription (admins only)
+  const userDocRef = doc(db, "users", userId);
+  const userDoc = await getDoc(userDocRef);
+  if (userDoc.exists()) {
+    const user = userDoc.data();
+    if (user?.role === "admin") {
+      return true;
+    }
+  }
 
   const subscriptionsRef = collection(db, "customers", userId, "subscriptions");
   const q = query(
